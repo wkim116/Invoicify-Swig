3b57a115a0d8776e794dae6467e034ada5acc1a8
Wan Kim <Wan.Kim@Ally.com>
2018-06-22 16:19:51 -0400

    Uploaded the InvoiceControllerTest file testing constructor only

--- src/test/java/com/invoicify/Invoices/InvoiceControllerTest.java
+++ src/test/java/com/invoicify/Invoices/InvoiceControllerTest.java
@@ -1,14 +1,25 @@
 package com.invoicify.Invoices;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.invoicify.Invoices.controllers.InvoiceController;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class InvoiceControllerTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDefaultConstructor() {
+		InvoiceController invoiceController = new InvoiceController();
+		assertThat(invoiceController, instanceOf(InvoiceController.class));
 	}
+	
+	
 
 }
