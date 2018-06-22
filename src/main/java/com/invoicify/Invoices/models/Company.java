9aa67f6667f4c933ce90239b5fabeccb95219830
Imranz96 <Imran.zahid@ally.com>
2018-06-22 10:21:13 -0400

    Invoice and Company class finished

--- src/main/java/com/invoicify/Invoices/models/Company.java
+++ src/main/java/com/invoicify/Invoices/models/Company.java
@@ -1,5 +1,55 @@
 package com.invoicify.Invoices.models;
 
-public class Company {
+import java.util.List;
 
-}
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "company")
+public class Company {
+	@Id
+	@GeneratedValue
+	
+	private int id;
+	private String name; 
+	
+	@OneToMany(mappedBy = "company")
+	private List<Invoice>invoice;
+
+	public Company() {}
+	
+	public Company(String name, List<Invoice>invoice) {
+		this.name = name;
+		this.invoice = invoice; 
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Invoice> getInvoice() {
+		return invoice;
+	}
+
+	public void setInvoice(List<Invoice> invoice) {
+		this.invoice = invoice;
+	}
+	
+	
+	}
--- src/main/java/com/invoicify/Invoices/models/Invoice.java
+++ src/main/java/com/invoicify/Invoices/models/Invoice.java
@@ -1,5 +1,82 @@
 package com.invoicify.Invoices.models;
 
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Invoice")
+
 public class Invoice {
+	
+	@Id
+	@GeneratedValue
+	
+	private int id; 
+	private Date createdOn;
+	private String invoiceDescription; 
+	
+	@OneToMany(mappedBy="invoices", cascade=CascadeType.ALL)
+	Set <InvoiceLineItem> lineItems; 
+	
+	@ManyToOne
+	Company company; 
+	
+	public Invoice () {}
+	
+	public Invoice (Date createdOn, String invoiceDescription,Set <InvoiceLineItem> lineItems, Company company) {
+		this.createdOn = createdOn;
+		this.invoiceDescription = invoiceDescription;
+		this.lineItems = lineItems; 
+		this.company = company;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public String getInvoiceDescription() {
+		return invoiceDescription;
+	}
+
+	public void setInvoiceDescription(String invoiceDescription) {
+		this.invoiceDescription = invoiceDescription;
+	}
+
+	public Set<InvoiceLineItem> getLineItems() {
+		return lineItems;
+	}
+
+	public void setLineItems(Set<InvoiceLineItem> lineItems) {
+		this.lineItems = lineItems;
+	}
+
+	public Company getCompany() {
+		return company;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+	
 
 }
