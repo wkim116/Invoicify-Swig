dff8d9b346276bccd9f39bfaff24949a1e73912c
Wan Kim <Wan.Kim@Ally.com>
2018-06-22 09:20:13 -0400

    Initial Commit, with only source files

---
+++ src/main/java/com/invocify/Invoices/InvocifySwigApplication.java
@@ -0,0 +1,12 @@
+package com.invocify.Invoices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvocifySwigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvocifySwigApplication.class, args);
+	}
+}
