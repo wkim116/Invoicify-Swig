af0982969856adcf2a84ea0aa6154d2ed561d26a
Wan Kim <Wan.Kim@Ally.com>
2018-06-26 09:54:16 -0400

    Pushing Imran's addition of the create mapping for the FlatFeeBillingRecordController

--- src/main/java/com/invoicify/Invoices/controllers/FlatFeeBillingRecordController.java
+++ src/main/java/com/invoicify/Invoices/controllers/FlatFeeBillingRecordController.java
@@ -1,16 +1,33 @@
 package com.invoicify.Invoices.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.invoicify.Invoices.models.Company;
 import com.invoicify.Invoices.models.FlatFeeBillingRecord;
+import com.invoicify.Invoices.models.User;
 import com.invoicify.Invoices.repositories.BillingRecordRepository;
+import com.invoicify.Invoices.repositories.CompanyRepository;
 
-@Controller
+@RestController
+@RequestMapping
 public class FlatFeeBillingRecordController {
 	
 	@Autowired
 	private BillingRecordRepository BillingRecordRepo;
+	
+	@Autowired
+	private CompanyRepository companyRepository;
+	
+	@Autowired
+	private BillingRecordRepository billingRepository;
+	
+	
 	
 	//Default Constructor
 	public FlatFeeBillingRecordController() {}
@@ -20,5 +37,21 @@ public class FlatFeeBillingRecordController {
 		FlatFeeBillingRecord flatFeeBillingRecord = new FlatFeeBillingRecord();
 		return flatFeeBillingRecord;
 	}
+	
+	// Post Mapping API to create a FlatFeeBillingRecord given a clientid
+	@PostMapping("{clientId}")
+	public FlatFeeBillingRecord create(@RequestBody FlatFeeBillingRecord record, @PathVariable int clientId, Authentication auth) {
+		User user = (User) auth.getPrincipal();
+		Company client = companyRepository.findOne(clientId);
+		record.setCompany(client);
+		record.setCreatedBy(user);
+		billingRepository.save(record);
+
+		return billingRepository.save(record);
+	} 
+ 
+
+	
+	
 
 }
